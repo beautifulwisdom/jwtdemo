bbe3a081fe770d8fccdecc612caa6563737a99dc
unknown <gnanar@india.claritrics.com>
2024-08-05 16:51:01 +0530

    Initial commit

---
+++ src/test/java/ai/buddi/gnana/jwt_demo/JwtDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.buddi.gnana.jwt_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
